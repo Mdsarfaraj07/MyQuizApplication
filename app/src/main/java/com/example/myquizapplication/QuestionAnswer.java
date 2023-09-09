d4bcded07ab5642f7f1cebaf4d118e5d4999ee17
md sarfaraj sah <mdsarfaraj7520@gmail.com>
2023-09-09 22:18:13 +0530

    first commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquizapplication;
+
+import kotlin.jvm.internal.Intrinsics;
+
+public class QuestionAnswer {
+
+    public static  String question[] ={
+           "which company owns the android?",
+           "which one is not the programming language?",
+           "which you are watching this video?",
+            "which Company owns the Apple?"
+    };
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+                    {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+    public static String correctAnswers[]={
+
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple"
+    };
+}
